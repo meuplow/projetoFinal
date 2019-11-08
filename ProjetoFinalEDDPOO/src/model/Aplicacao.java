c844c287fa9b2c78aafd0b88bb2efd60a8307901
Usuario <Usuario@Usuario-PC>
2019-11-08 19:38:39 -0300

    Lista de Pacientes em ordem alfabética concluída!
    Classe Aplicacao exemplificada

--- ProjetoFinalEDDPOO/src/model/Aplicacao.java
+++ ProjetoFinalEDDPOO/src/model/Aplicacao.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Aplicacao {
+	public static void main(String[] args) {
+		Paciente p1 = new Paciente("Maico", 03020305207, "19-04-01");
+		
+		ListaPaciente l1 = new ListaPaciente() ;
+		
+		Paciente p2 = new Paciente("Andre", 03020505207, "20-04-01");
+		l1.adiciona(p1);
+		l1.adiciona(p2);
+		
+		Paciente p3 = new Paciente("Tobias", 02220505207, "20-06-01");
+		l1.adiciona(p3);
+		
+		Paciente p4 = new Paciente("Rosa", 2020505207, "20-10-01");
+		l1.adiciona(p4);
+		
+		Paciente p5 = new Paciente("Ana", 2020505207, "20-10-01");
+		l1.adiciona(p5);
+		
+		l1.imprimirListaNome();
+		l1.imprimirPrimeiro();
 
+
+	}
 }
--- ProjetoFinalEDDPOO/src/model/ListaPaciente.java
+++ ProjetoFinalEDDPOO/src/model/ListaPaciente.java
@@ -14,34 +14,45 @@ public class ListaPaciente {
 		return false;
 	}
 
-//	public void adicionar(Paciente objeto) {
-//		NoPaciente novo = new NoPaciente(objeto);
-//		if (estaVazia()) {
-//			this.primeiro = novo;
-//		}
-//		else {
-//			NoPaciente aux = this.primeiro;
-//			while (aux.getProximo() != null) {
-//				aux = aux.getProximo();
-//			}
-//			aux.setProximo(novo);
-//		}
-//	}
+	public void adiciona(Paciente objeto) {
+		NoPaciente novo = new NoPaciente(objeto);
+		if (estaVazia()) {
+			this.primeiro = novo;
+		} else {
+			int comparacao = this.primeiro.getObjeto().getNome().compareTo(novo.getObjeto().getNome());
+			if (comparacao >= 0) { 	/* A condicao esta ">= 0", pois o retorno do metodo é em relacao ao primeiro
+									termo!! */
+				novo.setProximo(this.primeiro);
+				this.primeiro = novo;
+			} else {
+				NoPaciente aux = this.primeiro;
+				while (aux.getProximo() != null) {
+					comparacao = aux.getProximo().getObjeto().getNome().compareToIgnoreCase(novo.getObjeto().getNome());
+					if (comparacao >= 0) {
+						novo.setProximo(aux.getProximo());
+						aux.setProximo(novo);
+						break;
+					} else {
+						aux = aux.getProximo();
+					}
+				}
+				if (aux.getProximo() == null) {
+					aux.setProximo(novo);
+				}
+			}
+		}
+	}
 
-//	public void adiciona(Paciente objeto) {
-//		NoPaciente novo = new NoPaciente(objeto);
-//		if (estaVazia()) {
-//			this.primeiro = novo;
-//		} else {
-//			NoPaciente aux = this.primeiro.
-//			NoPaciente anterior = null;
-//			while (aux.getProximo() != null) {
-//				if(aux.getObjeto() > novo.getObjeto()) {
-//					break;
-//				}
-//			}
-//		}
-//
-//	}
+	public void imprimirListaNome() {
+		NoPaciente aux = this.primeiro;
+		while (aux != null) {
+			System.out.println(aux.getObjeto().getNome());
+			aux = aux.getProximo();
+		}
+	}
+
+	public void imprimirPrimeiro() {
+		System.out.println(this.primeiro.getObjeto().getNome());
+	}
 
 }
