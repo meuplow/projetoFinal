6c73e331a4299612e45ab72d1f20563070983912
Samsung <Samsung@Menin>
2019-11-29 19:39:22 -0300

    att

--- ProjetoFinalEDDPOO/src/control/PacienteControl.java
+++ ProjetoFinalEDDPOO/src/control/PacienteControl.java
@@ -238,10 +238,7 @@ public class PacienteControl implements ActionListener {
 					|| (filaPri5.size() > 0)) {
 				if (!filaPri1.isEmpty()) {
 					listaAtdEncerrados.adiciona(filaPri1.head().getObjeto());
-					System.out.println(filaPri1.head().getObjeto().getData());
-					System.out.println(filaPri1.head().getObjeto().retornaDateSaida());
-					filaPri1.head().getObjeto().imprimeComparacao();
-					filaPri1.head().getObjeto().imprimeDateFimConsulta();
+					filaPri1.head().getObjeto().imprimeComparacao(filaPri1.head().getObjeto().retornaHoraAtual(), filaPri1.head().getObjeto().getHora());
 					this.j.getTcha().getLblChamada().setText(Integer.toString(filaPri1.head().getObjeto().getSenha()));
 					this.j.getTcha().getLblChamada().setVisible(true);
 					filaPri1.dequeue();
